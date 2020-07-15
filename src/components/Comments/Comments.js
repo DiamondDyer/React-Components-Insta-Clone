5092791f130e30b6a4ad26910fa503556f029f0b
DiamondDyer <diamond.l.dyer@gmail.com>
2020-07-15 14:23:37 -0400

    finished

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,15 +2,25 @@ import React from 'react';
 import Comment from './Comment';
 import './Comments.css';
 
+
+
+
 const Comments = props => {
   // Make sure the parent of Comments is passing the right props!
-  const { comments } = props;
-
+  const {comments} = props;
+ 
+  console.log(comments)
   return (
     <div>
-      {/* map through the comments data array and render a Comment for every comment piece of data */}
+      {/* map through the comments data array and render a Comment for every comment piece of data */
+       comments && comments.map(comment=>{
+         return <Comment key = {comment.id} comment = {comment}/>
+       })
+       
+      }
     </div>
   );
+  
 };
 
 export default Comments;
--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -4,11 +4,72 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
+const dummyData = [
+  {
+      id: 1,
+      username: "philzcoffee",
+      thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+      imageUrl: 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80',
+      likes: 400,
+      timestamp: "July 17th 2017, 12:42:40 pm",
+      comments: [
+          {
+              id: 22,
+              username: "philzcoffee",
+              text: "We've got more than just coffees!",
+          },
+          {
+              id: 23,
+              username: "biancasaurus",
+              text: "Looks delicious!",
+          },
+          {
+              id: 24,
+              username: "martinseludo",
+              text: "Can't wait to try it!",
+          },
+      ],
+  },
+  {
+      id: 2,
+      username: "twitch",
+      thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+      imageUrl: 'https://images.unsplash.com/photo-1566577134770-3d85bb3a9cc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80',
+      likes: 4307,
+      timestamp: "July 15th 2017, 03:12:09 pm",
+      comments: [
+          {
+              id: 36,
+              username: "twitch",
+              text: "Epic Street Fighter action here in Vegas!",
+          },
+          {
+              id: 37,
+              username: "michaelmarzetta",
+              text: "Omg that match was crazy",
+          },
+          {
+              id: 38,
+              username: "themexican_leprechaun",
+              text: "What a setup",
+          },
+          {
+              id: 39,
+              username: "dennis_futbol",
+              text: "It that injustice",
+          },
+      ],
+  },
+];
+
+
+
 
 const LikeSection = props => {
   // Make sure the parent of LikeSection is passing the right props!
-  const { likePost, likeCount } = props
+  const { likePost, likeCount, likes, post } = props
 
+  console.log(props)
   return (
     <div>
       <div
@@ -16,13 +77,13 @@ const LikeSection = props => {
         key="likes-icons-container"
       >
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon onClick = {evt => likePost(post)}icon={faHeart} />
         </div>
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faComment} />
+          <FontAwesomeIcon  icon={faComment} />
         </div>
       </div>
-      <p className="like-number">100 likes</p>
+      <p className="like-number">{likeCount}  likes</p>
     </div>
   )
 };
